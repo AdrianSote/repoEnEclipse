cf06290fed9d1829a8e9e794e27bef9fbbe9df3e
AdrianSote <AdrianSote>
2023-03-20 20:08:45 +0100

    Primer mensaje 20-03-2023 Adrian V.

---
+++ project_GIT/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Adri
+ *
+ */
+module project_GIT {
+}
---
+++ project_GIT/src/project_GIT/Ejemplo.java
@@ -0,0 +1,12 @@
+package project_GIT;
+
+public class Ejemplo {
+
+	/**
+	 * Pre:---
+	 * Post:
+	 */
+	public static void main(String[] args) {
+		System.out.println("Prueba en GitHub");
+	}
+}
